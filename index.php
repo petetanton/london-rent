03b11324b470e8f9b020da8c67614477a9b82379
Pete Tanton <tantop01@MC-S097621.local>
2015-09-29 23:11:48 +0100

    Correcting typo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
-include 'src/DatabaseControl';
+include 'src/DatabaseControl.php';
 $zooplaKey = getenv("ZOOPLA_KEY");
 $dbControl = new DatabaseControl();
 var_dump($dbControl->getStationsByZone("1"));
