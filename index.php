c05c792523759bfb3da66e0ccaa1967499ceee29
Pete Tanton <tantop01@MC-S097621.local>
2015-09-29 23:33:15 +0100

    Setup of db

--- index.php
+++ index.php
@@ -13,7 +13,10 @@ $client = DynamoDbClient::factory(array(
 ));
 $response = $client->query(array(
     'TableName' => 'station_info',
-    'KeyConditionExpression' => 'london_zone = 1'
+    'KeyConditionExpression' => 'london_zone = :v_id',
+    'ExpressionAttributeValues' =>  array (
+        ':v_id' => array('S' => '1')
+      )
     )
 ));
 
