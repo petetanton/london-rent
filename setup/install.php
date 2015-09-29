3dd92314ac0fdfd3a17a33abaeb18e8f9a8fe495
Pete Tanton <tantop01@MC-S097621.local>
2015-09-29 14:00:06 +0100

    AWS SDK

---
+++ setup/install.php
@@ -0,0 +1,40 @@
+<?php
+require "../src/aws/aws-autoloader.php";
+use Aws\DynamoDb\DynamoDbClient;
+$client = DynamoDbClient::factory(array(
+  'profile' => 'default',
+  'region' => 'eu-west-1',
+));
+
+
+$row = 1;
+if (($handle = fopen("stations_locations.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $stationName[] = $data[0];
+        $mapX[] = $data[1];
+        $mapY[] = $data[2];
+        $latitude[] = $data[3];
+        $longitude[] = $data[4];
+        $londonZone[] = $data[5];
+        $postCode[] = $data[6];
+      $row++;
+    }
+    fclose($handle);
+}
+for($i=1;$i<count($stationName)+1;$i++) {
+  $result = $client->putItem(array(
+    'TableName' => 'station_info',
+    'Item' => array(
+          'station_id'      => array('N' => $i),
+          'station_name' => array('S' => $stationName[$i]),
+          'map_x' => array('N' => $mapX[$i]),
+          'map_y' => array('N' => $mapY[$i]),
+          'latitude' => array('N' => $latitude[$i]),
+          'longitude' => array('N' => $longitude[$i]),
+          'london_zone' => array('N' => $londonZone[$i]),
+          'post_code' => array('S' => $postCode[$i]) 
+      )
+  ));
+}
+
+?>
