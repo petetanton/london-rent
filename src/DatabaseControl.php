4d20d1a2d7536d4967abab0b2dc34c2417335def
Pete Tanton <tantop01@MC-S097621.local>
2015-09-29 23:16:38 +0100

    Update to DatabaseControl class

--- src/DatabaseControl.php
+++ src/DatabaseControl.php
@@ -3,14 +3,15 @@ require "/aws/aws-autoloader.php";
 require "../../cred.php";
 use Aws\DynamoDb\DynamoDbClient;
 class DatabaseControl {
-  var $client = new DynamoDbClient::factory(array(
-    'key'    => $aws_key,
-    'secret' => $aws_secret,
-    'region' => 'eu-west-1'
-  ));
+
 
   function getStationsByZone($zone) {
-    $response = $this->client->getItem(array(
+     $client = DynamoDbClient::factory(array(
+      'key'    => $aws_key,
+      'secret' => $aws_secret,
+      'region' => 'eu-west-1'
+    ));
+    $response = $client->getItem(array(
       'TableName' => 'station_info',
       'Key' => array(
           'london_zone' => array( 'S' => $zone )
