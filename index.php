65173d5d065e09d52e362b7478393888b2150444
Pete Tanton <tantop01@MC-S097621.local>
2015-09-29 23:20:11 +0100

    Setup of db

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
-include 'src/DatabaseControl.php';
+require "src/aws/aws-autoloader.php";
+require "../cred.php";
+use Aws\DynamoDb\DynamoDbClient;
 $zooplaKey = getenv("ZOOPLA_KEY");
-$dbControl = new DatabaseControl();
-var_dump($dbControl->getStationsByZone("1"));
+$client = DynamoDbClient::factory(array(
+ 'key'    => $aws_key,
+ 'secret' => $aws_secret,
+ 'region' => 'eu-west-1'
+));
+$response = $client->getItem(array(
+ 'TableName' => 'station_info',
+ 'Key' => array(
+     'london_zone' => array( 'S' => $zone )
+   )
+ ));
+var_dump($response);
 
 ?>
