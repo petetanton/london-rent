e509ca3bd356f80c002c8cfd6a869696cf9b3ccf
Pete Tanton <tantop01@MC-S097621.local>
2015-09-29 22:00:38 +0100

    imporve deploy script

--- setup/deploy.php
+++ setup/deploy.php
@@ -1,5 +1,6 @@
 <?php
 echo shell_exec('cd /var/www/html');
 echo '<br>';
-echo shell_exec('sudo -Hu apache git pull origin master');
+echo shell_exec('sudo -Hu apache git fetch origin');
+echo shell_exec('sudo -Hu apache git reset --hard origin/master')
 ?>
